4f1a4e2055f4793c73163ae372942fa874d7a3f4
Konrad Maciałek <k.macialek@gmail.com>
2019-03-22 14:38:06 +0100

    Single testrun json parsing completed.

--- Parser.cs
+++ Parser.cs
@@ -1,15 +1,46 @@
-using System;
-using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
 using Newtonsoft.Json.Linq;
 
 namespace tfs
 {
     public class Parser
     {
-        public void Parse(string singleTestRun)
+        private const string testCaseTitleNode = "testCaseTitle";
+        private const string testContainerNode = "automatedTestStorage";
+
+        private readonly Dictionary<string, List<string>> _testsCollection;
+
+        public Parser(Dictionary<string, List<string>> testsCollection)
         {
-            var parsedObject = JObject.Parse(singleTestRun);
+            _testsCollection = testsCollection;
         }
-        
+
+        public Dictionary<string, List<string>> Parse(string singleTestRunJson)
+        {
+            using (StreamReader r = new StreamReader(singleTestRunJson))
+            {
+                string json = r.ReadToEnd();
+                JObject items = JObject.Parse(json);
+                JArray values = (JArray)items["value"];
+
+                foreach (var value in values)
+                {
+                    var title = value[testCaseTitleNode].ToString();
+                    var container = value[testContainerNode].ToString();
+
+                    if (_testsCollection.ContainsKey(container))
+                    {
+                        _testsCollection[container].Add(title);
+                    }
+                    else
+                    {
+                        _testsCollection.Add(container, new List<string> { title });
+                    }
+                }
+            }
+
+            return _testsCollection;
+        }
     }
 }
